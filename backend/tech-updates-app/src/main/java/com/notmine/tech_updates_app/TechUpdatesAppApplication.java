d1323a66a32e39c7505786dd22499fd0f8e2bf83
PRAJWAL9597 <prajwals9597@gmail.com>
2025-07-24 18:24:36 +0530

    Initializing the project and setup README.md and structure

---
+++ backend/tech-updates-app/src/main/java/com/notmine/tech_updates_app/TechUpdatesAppApplication.java
@@ -0,0 +1,13 @@
+package com.notmine.tech_updates_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechUpdatesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechUpdatesAppApplication.class, args);
+	}
+
+}
---
+++ backend/tech-updates-app/src/test/java/com/notmine/tech_updates_app/TechUpdatesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notmine.tech_updates_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechUpdatesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
