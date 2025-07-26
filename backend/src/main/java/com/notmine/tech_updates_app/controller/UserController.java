f29681122fb567fd2500d87c72d9dd51be597042
PRAJWAL9597 <prajwals9597@gmail.com>
2025-07-26 16:27:19 +0530

    Succesfully created an entry in database changed port to 8081 and updated the UserController

--- backend/src/main/java/com/notmine/tech_updates_app/controller/UserController.java
+++ backend/src/main/java/com/notmine/tech_updates_app/controller/UserController.java
@@ -1,23 +1,29 @@
 package com.notmine.tech_updates_app.controller;
 
 import com.notmine.tech_updates_app.model.User;
+import com.notmine.tech_updates_app.repository.UserRepository; 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-        @PostMapping("/register")
-        public String registerUser(@RequestBody User newUser) {
-            System.out.println("Recived Registration Request for : "+newUser.getUserName());
-            System.out.println("Email"+ newUser.getEmail());
 
-            return "User "+newUser.getUserName()+" registered successfully";
-        }
+   
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User newUser) {
         
-
+        User savedUser = userRepository.save(newUser);
+        
+        System.out.println("Saved user to database with ID: " + savedUser.getId());
+        
+        
+        return savedUser;
+    }
 }
