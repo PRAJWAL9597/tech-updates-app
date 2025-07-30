746adeda25d70369be7eda7af49f80d19374ac2d
PRAJWAL9597 <prajwals9597@gmail.com>
2025-07-30 21:58:58 +0530

    adding password util for hashing the passwords and created dto for login function

--- backend/src/main/java/com/notmine/tech_updates_app/repository/UserRepository.java
+++ backend/src/main/java/com/notmine/tech_updates_app/repository/UserRepository.java
@@ -3,11 +3,15 @@ package com.notmine.tech_updates_app.repository;
 import com.notmine.tech_updates_app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
 
 @Repository // marks thsis a spring repo bean
+
 public interface UserRepository extends JpaRepository<User, Long> {
         //spring data jpa will automatically create database methods for me
         //hehe
-    
+ Optional<User> findByEmail(String email);   
 }
 
